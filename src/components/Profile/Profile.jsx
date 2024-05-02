604dc32b124edfeb3bf910366e0d68e38e14334d
Tanish Mishra <tanishmishra97@gmail.com>
2024-05-03 02:40:12 +0530

    fix: profile overlay added

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,69 +1,63 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Profile.module.css";
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 import ToastMessage from "../../utils/toastMessage";
 import { getblogs } from "../../apis/blogs";
 
 import ProfilePhoto from "/assets/images/profile_pic.png";
-import dashboardIcon from "/assets/icons/dashboard.png"; 
-import plusIcon from "/assets/icons/plus.png"; 
-import documentIcon from "/assets/icons/document.png"; 
-import profileIcon from "/assets/icons/profile.png"; 
+import dashboardIcon from "/assets/icons/dashboard.png";
+import plusIcon from "/assets/icons/plus.png";
+import documentIcon from "/assets/icons/document.png";
+import profileIcon from "/assets/icons/profile.png";
 import settingsIcon from "/assets/icons/settings.png";
-import exitIcon from '/assets/icons/exit.png'
-
+import exitIcon from "/assets/icons/exit.png";
 
 const Profile = (props) => {
-    const [isSelected,setIsSelected] = useState(3)
-    const [screenSize,setScreenSize] = useState(window.innerWidth)
-    const [navigationData] = useState([
-        {
-            id: 1,
-            icon: dashboardIcon,
-            title: "Dashboard",
-        },
-        {
-            id: 2,
-            icon: plusIcon,
-            title: "Add new Post",
-        },
-        {
-            id: 3,
-            icon: documentIcon,
-            title: "Your Feeds",
-        },
-        {
-            id: 4,
-            icon: profileIcon,
-            title: "Your Saved",
-        },
-        {
-            id: 5,
-            icon: settingsIcon,
-            title: "Profile",
-        }
-    ]);
+  const [isSelected, setIsSelected] = useState(3);
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+  const [navigationData] = useState([
+    {
+      id: 1,
+      icon: dashboardIcon,
+      title: "Dashboard",
+    },
+    {
+      id: 2,
+      icon: plusIcon,
+      title: "Add new Post",
+    },
+    {
+      id: 3,
+      icon: documentIcon,
+      title: "Your Feeds",
+    },
+    {
+      id: 4,
+      icon: profileIcon,
+      title: "Your Saved",
+    },
+    {
+      id: 5,
+      icon: settingsIcon,
+      title: "Profile",
+    },
+  ]);
 
-    const checkScreenSize = () => {
-        setScreenSize(window.innerWidth)
-    }
-    window.addEventListener("resize",checkScreenSize)
-  
-
+  const checkScreenSize = () => {
+    setScreenSize(window.innerWidth);
+  };
+  window.addEventListener("resize", checkScreenSize);
 
   return (
-    <>
-    <div className={styles.profile}
-  
-     style={{
-      display: props.isProfileActive || screenSize > 430 ? "flex" : "none",
 
-     }}
+    <div
+     
+      className={styles.profile}
+      style={{
+        display: props.isProfileActive || screenSize > 430 ? "flex" : "none",
+      }}
     >
-  <Toaster
-  position="top-right"
-  reverseOrder={false}
-/>
+      <Toaster position="top-right" reverseOrder={false} />
       <div className={styles.profile__information}>
         <img
           src={ProfilePhoto}
@@ -83,43 +77,51 @@ const Profile = (props) => {
         </div>
       </div>
 
-        <div className={styles.profile__navigation}>
-          <ul className={styles.profile__navigation_list}>
-
-            {
-                navigationData?.map((item)=>(
-                    <li onClick={()=>{
-                        setIsSelected(item.id)
-                        if(item.id !==3) {
-                          ToastMessage("Under Development!",1)
-                        }
-                    }} key={item.id}>
-                    <img src={item.icon} alt={item.title} loading="lazy" />
-                    <span style={{
-                        color: isSelected === item.id ? "#3399F0" : "",
-                    }}> {item.title} </span>
-                    </li>
-                ))
-            }
-           
-          </ul>
-             </div>
-  <div className={styles.profile__logout} onClick={()=>{
-    if(!props.isProfileActive){
-      ToastMessage("Under Development!",1)
-    }
-  }}><img src={exitIcon} alt="exit" /> <span onClick={()=>{
-     props.setIsProfileActive(false)
-  }}>Log Out</span></div>
-
+      <div className={styles.profile__navigation}>
+        <ul className={styles.profile__navigation_list}>
+          {navigationData?.map((item) => (
+            <li
+              onClick={() => {
+                setIsSelected(item.id);
+                if (item.id !== 3) {
+                  ToastMessage("Under Development!", 1);
+                }
+              }}
+              key={item.id}
+            >
+              <img src={item.icon} alt={item.title} loading="lazy" />
+              <span
+                style={{
+                  color: isSelected === item.id ? "#3399F0" : "",
+                }}
+              >
+                {" "}
+                {item.title}{" "}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div
+        className={styles.profile__logout}
+        onClick={() => {
+          if (!props.isProfileActive) {
+            ToastMessage("Under Development!", 1);
+          }
+        }}
+      >
+        <img src={exitIcon} alt="exit" />{" "}
+        <span
+          onClick={() => {
+            props.setIsProfileActive(false);
+          }}
+        >
+          Log Out
+        </span>
+      </div>
     </div>
 
-  <div className={styles.overlay}></div>
-
-</>
-
   );
-  
 };
 
 export default Profile;
